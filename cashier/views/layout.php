09754f6bfba293357fa48343c112943ef85ad71c
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-21 00:51:43 +0800

    Update layout.php

--- cashier/views/layout.php
+++ cashier/views/layout.php
@@ -195,12 +195,12 @@
                         Email Admin
                     </a>
                 </li>
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'view_shifts.php' ? 'active' : ''; ?>" href="view_shifts.php">
                         <i class="fas fa-calendar-alt"></i>
                         View Shifts
                     </a>
-                </li>
+                </li> -->
                 <li class="nav-item">
                     <a class="nav-link <?php echo basename($_SERVER['PHP_SELF']) == 'account_settings.php' ? 'active' : ''; ?>" href="account_settings.php">
                         <i class="fas fa-user-cog"></i>
